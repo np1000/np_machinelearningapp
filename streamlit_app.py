450d138fd268a737ba7faabbd93ec566e1d93a84
np1000 <neelima257@yahoo.com>
2025-01-17 15:02:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🎈 Machine learning App')
 
-st.info('This app uses machine learning model')
+st.info('This app builds a machine learning model')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
