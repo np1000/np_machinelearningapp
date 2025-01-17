df75af8baaf641e7ba4d02034e2849353db0dfac
np1000 <neelima257@yahoo.com>
2025-01-17 15:12:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ with st.expander('Data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
 
-st.write('**X**'):
+st.write('**X**')
 df.drop('species',axis=1)
 X
 
