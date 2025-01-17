d4018a19a9af888036456af7115724c725f22d5a
np1000 <neelima257@yahoo.com>
2025-01-17 16:42:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,3 +58,17 @@ df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 X = df_penguins[1:]
 input_row = df_penguins[:1]
 
+# Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
+def target_encode(val):
+  return target_mapper[val]
+
+y = y_raw.apply(target_encode)
+
+with st.expander('Data preparation'):
+  st.write('**Encoded X (input penguin)**')
+  input_row
+  st.write('**Encoded y**')
+  y
