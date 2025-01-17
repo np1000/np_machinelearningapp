579f43920949e570194d99b744c2cce5b58b50d9
np1000 <neelima257@yahoo.com>
2025-01-17 15:22:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,4 @@ y = df.species
 y
 #species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex#
 with st.expander('Data Visulaisation'):
-  st.scatter_chart(data=df, x='bill_length_mm', y= 'body_mass_g' color ='species')
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color ='species')
