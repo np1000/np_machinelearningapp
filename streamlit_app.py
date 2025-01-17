a16ed319921fd728e39468cfdca871ffbb0fb24b
np1000 <neelima257@yahoo.com>
2025-01-17 16:18:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,13 @@ y
 #species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex#
 with st.expander('Data Visulaisation'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color ='species')
+
+# Input features
+with st.sidebar:
+  st.header('Input features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
